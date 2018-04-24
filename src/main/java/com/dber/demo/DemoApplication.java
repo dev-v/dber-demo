c9e18725d88b28b5f6e255db277099e3c5d61c97
dev-v <250912570@qq.com>
2018-04-24 17:56:32 +0800

    提交

---
+++ src/main/java/com/dber/demo/DemoApplication.java
@@ -0,0 +1,15 @@
+package com.dber.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableAutoConfiguration
+public class DemoApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
